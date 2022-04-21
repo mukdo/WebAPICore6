ca9e82f2a69a01fc1395ecbcf50e52805a7897ec
Mukdo <bmukdo@gmail.com>
2022-04-21 12:47:39 +0600

    Add project files.

---
+++ Common/Notepad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Notepad
+    {
+        public int Id { get; set; }
+        public string Note { get; set; }
+    }
+}
---
+++ WebCore6.API/Controllers/NoteController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebCore6.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : ControllerBase
+    {
+        // GET: api/<NoteController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<NoteController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<NoteController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<NoteController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<NoteController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
